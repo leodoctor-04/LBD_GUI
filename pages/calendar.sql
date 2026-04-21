cb45769ad15ffaa3a52a626f5071d7d08f12adef
dm <dario.mugnaini10@gmail.com>
2026-04-22 00:18:58 +0200

    aggiunta query + simulazione riempimento day clm

--- pages/calendar.sql
+++ pages/calendar.sql
@@ -1,6 +1,21 @@
 create or replace procedure calendar(startDate in date default null )AS
-    type dname_array is varray(5) of varchar(10);
-    dname dname_array := dname_array('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday');
+    -- constants
+    dayRange constant number := 7;
+
+    -- types
+    type dname_array is varray(dayRange) of varchar(10);
+    type lesson is record(
+        course_title varchar(100), -- mettere type of
+        instructor_name varchar(100),
+        instructor_surname varchar(100),
+        startD date
+    );
+    type lesson_list is table of lesson;
+    type dayXlessons is varray(dayRange) of lesson_list;
+
+
+    -- variables
+    dname dname_array := dname_array('Lunedi', 'Martedi', 'Mercoledi', 'Giovedi', 'Venerdi','Sabato','Domenica');
 
     toolPn panel := Panel(
         css_style => layout.HLIST('10px',aligment.page_center)|| 'background:red'
@@ -9,7 +24,52 @@ create or replace procedure calendar(startDate in date default null )AS
         css_style => layout.HLIST('',aligment.page_start)|| 'background:blue;' || 'height:100%;'
     );
     currP panel;
+    currLessClm panel;
     nxt_monday date;
+    lessons dayXlessons;
+
+    -- functions & procedures 
+    function lesson_toArray return dayXlessons is
+        monday date;
+        d number; 
+        res dayXlessons := dayXlessons(); 
+    begin
+
+        --generate tables
+        for i in 1 .. dayRange loop
+            res.extend;
+            res(i) := lesson_list();
+        end loop;
+
+        --add lesson to the table
+        for c_row in (
+            SELECT corso.titolo,utente.nome,utente.cognome,lezione.DATAINIZIO
+            FROM lezione,corso,utente
+            WHERE 
+                --join
+                lezione.idCorso = corso.idCorso and
+                corso.idistruttore =  utente.idutente and
+                -- data check
+                lezione.datainizio between to_date('01-jun-26','dd-mon-yy') and to_date('30-jun-26','dd-mon-yy')
+            ORDER BY lezione.datainizio,lezione.datafine
+            )
+        loop
+            monday := NEXT_DAY(c_row.datainizio-7, 'MONDAY');
+            d := (c_row.datainizio - monday ) + 1;
+            dbms_output.put_line(d);
+            res(d).extend;
+            res(d)(res(d).count) := lesson(
+                course_title  => c_row.titolo,
+                instructor_name  => c_row.nome,
+                instructor_surname => c_row.cognome,
+                startD => c_row.datainizio
+            );
+            
+        end loop;
+
+        return res;
+    end;
+
 BEGIN
     -- setup 
     if (startDate is null) then
@@ -17,8 +77,7 @@ BEGIN
     else
         nxt_monday := NEXT_DAY(startDate-7, 'MONDAY');
     end if;
-
-    --- query
+    lessons := lesson_toArray;
 
     -- ui
     basehtml.apriPagina( titolo => 'calendar');
@@ -39,7 +98,7 @@ BEGIN
     ));
     toolpn.showhtml;
 
-    for i in  1 .. 5 loop
+    for i in  1 .. dayRange loop
         -- generate day column
         currP := panel(
                 css_style => layout.vlist('0px') || 'background:blue;' || 'flex-grow:1;'
@@ -47,15 +106,23 @@ BEGIN
 
         currP.add_element(label(css_style => 'text-align:center', text => dname(i)));
         ---- add lessons
-        currp.add_element(
-            panel(
-                css_style => layout.vlist('5px',aligment.page_center,aligment.page_start) || 'background:yellow;' || 'min-height:20vw;' 
-            )
+        currLessClm := panel(
+            css_style => layout.vlist('5px',aligment.page_start,aligment.page_start) || 'background:yellow;' || 'min-height:20vw;' 
         );
 
+        for j in 1 .. lessons(i).count loop
+            currLessClm.add_element(
+                Label(text => 'carlito')
+            );
+        end loop;
+
+        currp.add_element(
+            currLessClm
+        );
         -- add to the result
         days.add_element(currP);
     end loop;
+
     days.showhtml;
 
     basehtml.chiudiPagina;
