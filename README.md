4391eb5629a955aa234b19dccf91e2c0da706248
dm <dario.mugnaini10@gmail.com>
2026-04-28 17:18:55 +0200

    aggiornato il readme + script per il drop dei tipi

--- README.md
+++ README.md
@@ -21,6 +21,18 @@ steps:
 9. pages/loginProc.sql
 10. grant.sql
 
+# sequenza di compilazione ui_v2
+1. drop dei tipi, drop_types.sql (non sempre necessario ma per sicurezza fatelo)
+2. compilare ui_v2/uielem.sql
+3. compilare ui_v2/container.sql
+4. compilare ui_v2/inputs.sql
+5. compilare ui_v2/multiOpt_input.sql 
+5. compilare ui_v2/tables.sql 
+6. compilare i pacchetti di supporto(enum virtuali):
+    - ui/aligments.sql
+    - ui/layout.sql
+    - ui_v2/inputTypes.sql
+
 # esempi
 
 esempio di url: http://131.114.73.17:8080/apex/Benedetti2526.home
---
+++ drop_types.sql
@@ -0,0 +1,40 @@
+--uielem
+drop type html_link     FORCE; 
+drop type label         FORCE; 
+drop type button        FORCE;
+drop type uielem        FORCE;
+
+-- container
+drop type popup         FORCE;
+drop type input_form    FORCE;
+drop type panel         FORCE;
+drop type ui_container  FORCE;
+
+-- inputs
+drop type timeInput     FORCE;
+drop type dateInput     FORCE;
+drop type checkbox      FORCE;
+drop type textAreaInput FORCE;
+drop type passwordInput FORCE;
+drop type textInput     FORCE;
+drop type numberInput   FORCE;
+drop type submit_butt   FORCE;
+drop type f_input       FORCE;
+
+-- multiOpt_input
+drop type multioptionInput  FORCE;
+drop type option_menu       FORCE;
+drop type radioOptionsInput FORCE;
+
+-- tables
+drop type trow          FORCE;
+drop type data_table    FORCE;
+
+
+-- utils
+drop type trow_list         FORCE;
+drop type strings           FORCE;
+drop type element_list      FORCE;
+drop type ora               FORCE;
+drop type string_pair       FORCE;
+DROP TYPE option_list       FORCE;
